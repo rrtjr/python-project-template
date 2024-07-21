ac5b2341a900e65f9d70670b6c115a9e89140298
rrtjr <rey.tugade@gmail.com>
2024-07-21 15:13:41 +0800

    Added setup_project.py

---
+++ setup_project.py
@@ -0,0 +1,57 @@
+"""
+This module provides functions to change placeholders into actual project information.
+"""
+
+import os
+import re
+
+
+def replace_in_file(file_path: str, old_string: str, new_string: str) -> None:
+    """
+    Replaces occurrences of a string in a file with a new string.
+
+    Args:
+        file_path (str): The path to the file.
+        old_string (str): The string to be replaced.
+        new_string (str): The string to replace with.
+    """
+    with open(file_path, "r", encoding="utf-8") as file:
+        content: str = file.read()
+    content = re.sub(old_string, new_string, content)
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write(content)
+
+
+def rename_project_directory(old_name: str, new_name: str) -> None:
+    """
+    Renames a project directory from an old name to a new name.
+
+    Args:
+        old_name (str): The current name of the project directory.
+        new_name (str): The new name for the project directory.
+    """
+    os.rename(f"src/{old_name}", f"src/{new_name}")
+
+
+def main() -> None:
+    """
+    Main function to update the project name in files and rename the project directory.
+    """
+    old_name = "python-project-template-by-rtj"
+    new_name: str = input("Enter the new project name: ")
+
+    files_to_update = [
+        "pyproject.toml",
+        f"src/{old_name}/__init__.py",
+    ]
+
+    for file_path in files_to_update:
+        replace_in_file(file_path, old_name, new_name)
+
+    rename_project_directory(old_name, new_name)
+
+    print(f"Project name has been updated to {new_name}")
+
+
+if __name__ == "__main__":
+    main()
